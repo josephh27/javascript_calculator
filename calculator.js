499294ca3830411855ed2e21b35f3f1e35982017
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-01-10 00:14:21 +0800

    initial addition of files

---
+++ calculator.js
@@ -0,0 +1,165 @@
+// const myButtons = document.querySelector(".calculator-keys")
+
+// const calculator = {
+//     display: '0',
+//     firstOperand: null,
+//     waitingForSecondOperand: false,
+//     operator: null
+
+// }
+
+// function inputDigit(value){
+//     const {displayValue, waitingForSecondOperand} = calculator
+//     if (waitingForSecondOperand){
+//         calculator.displayValue = value;
+//         waitingForSecondOperand = false;
+//     } else {
+//         calculator.displayValue = displayValue === "0" ? value : displayValue + value;
+//     }
+// }
+
+// function handleOperator(secondOperator){
+
+// }
+
+
+// myButtons.addEventListener("click", (event) => {
+//     const {target} = event
+//     const {value} = target
+//     if(!target.matches(button)){
+//         return;
+//     }
+//     switch (value){
+//         case '+': 
+//          handleOperator(value)
+//         default:
+//         if (Number.isInteger(parseFloat(value))){
+//             inputDigit(value)
+//         }
+//     }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+  };
+  
+  function inputDigit(digit) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+  
+    if (waitingForSecondOperand === true) {
+      calculator.displayValue = digit;
+      calculator.waitingForSecondOperand = false;
+    } else {
+      calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
+    }
+  }
+  
+  function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) {
+        calculator.displayValue = "0."
+      calculator.waitingForSecondOperand = false;
+      return
+    }
+  
+    if (!calculator.displayValue.includes(dot)) {
+      calculator.displayValue += dot;
+    }
+  }
+  
+  function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator
+    const inputValue = parseFloat(displayValue);
+    
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
+  
+  
+    if (firstOperand == null && !isNaN(inputValue)) {
+      calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
+    }
+  
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+  }
+  
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
+  
+  function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+  }
+  
+  function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+  }
+  
+  updateDisplay();
+  
+  const keys = document.querySelector('.calculator-keys');
+  keys.addEventListener('click', event => {
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches('button')) {
+      return;
+    }
+  
+    switch (value) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '=':
+        handleOperator(value);
+        break;
+      case '.':
+        inputDecimal(value);
+        break;
+      case 'all-clear':
+        resetCalculator();
+        break;
+      default:
+        
+          inputDigit(value);
+    
+    }
+  
+    updateDisplay();
+  });
+  
