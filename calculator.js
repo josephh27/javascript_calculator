b526d4fa2af02c886a41a70716551eda88d883c4
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-01-16 00:26:50 +0800

    adding decimal to fraction and vice versa conversion, also added squaring feature.

--- calculator.js
+++ calculator.js
@@ -1,57 +1,193 @@
 const myButtons = document.querySelector(".calculator-keys")
-
+const fraction = document.querySelector("#fraction")
 const calculator = {
-    display: '0',
+    display: "0",
     firstOperand: null,
     waitingForSecondOperand: false,
     operator: null
-
 }
 
 function inputDigit(value){
-    const {displayValue, waitingForSecondOperand} = calculator
+    const {display, waitingForSecondOperand} = calculator
     if (waitingForSecondOperand){
-        calculator.displayValue = value;
-        waitingForSecondOperand = false;
+        calculator.display = value;
+        calculator.waitingForSecondOperand = false;
     } else {
-        calculator.displayValue = displayValue === "0" ? value : displayValue + value;
+        calculator.display = display === "0" ? value : display + value;
     }
 }
 
-function calculate()
+function greatest_common_factor(x, y){
+    if (!y){
+        return x
+    } return greatest_common_factor(y, x % y)
+}
+
+function calculate(firstOperand, secondOperand, operator){
+    let multiplier = 10**4
+    if (operator === "+"){
+        const sum = parseFloat((firstOperand + secondOperand).toFixed(4))
+        if (!Number.isInteger(sum)){
+            common_factor = Math.abs(greatest_common_factor(sum*multiplier, 1*multiplier))
+            numerator = (sum*multiplier)/common_factor
+            denominator = (1*multiplier)/common_factor
+            fraction.innerHTML = `<sup>${numerator}</sup>&frasl;<sub>${denominator}</sub>`
+            calculator.display = fraction.textContent 
+        } else{
+            calculator.display = firstOperand + secondOperand
+        }
+        return firstOperand + secondOperand
+    } else if (operator === "-"){
+        const difference = parseFloat((firstOperand - secondOperand).toFixed(4))
+        if (!Number.isInteger(difference)){
+            common_factor = Math.abs(greatest_common_factor(difference*multiplier, 1*multiplier))
+            console.log(common_factor)
+            numerator = (difference*multiplier)/common_factor
+            denominator = (1*multiplier)/common_factor
+            fraction.innerHTML = `<sup>${numerator}</sup>&frasl;<sub>${denominator}</sub>`
+            calculator.display = fraction.textContent 
+        } else{
+            calculator.display = firstOperand - secondOperand
+        }
+        
+        return firstOperand - secondOperand
+    } else if (operator === "*"){
+        const product = parseFloat((firstOperand * secondOperand).toFixed(4))
+        if (!Number.isInteger(product)){
+            common_factor = Math.abs(greatest_common_factor(product*multiplier, 1*multiplier))
+            numerator = (product*multiplier)/common_factor
+            denominator = (1*multiplier)/common_factor
+            fraction.innerHTML = `<sup>${numerator}</sup>&frasl;<sub>${denominator}</sub>`
+            calculator.display = fraction.textContent 
+        } else{
+            calculator.display = firstOperand * secondOperand
+        }
+        return firstOperand * secondOperand
+    } else if (operator === "/"){
+        const quotient = parseFloat((firstOperand / secondOperand))
+        if (!Number.isInteger(firstOperand)){
+            multiplier = 10**(((firstOperand.toString()).split("."))[1].length)
+        }
+        if (!Number.isInteger(quotient)){
+            numerator = (parseFloat(firstOperand)*multiplier)
+            denominator = (secondOperand*multiplier)
+            common_factor = Math.abs(greatest_common_factor(numerator, denominator))
+            fraction.innerHTML = `<sup>${numerator/common_factor}</sup>&frasl;<sub>${denominator/common_factor}</sub>`
+            calculator.display = fraction.textContent 
+        } else{
+            calculator.display = firstOperand / secondOperand
+        }
+        return firstOperand / secondOperand
+    } else if (operator === "**2"){
+        const squared = parseFloat((firstOperand ** 2).toFixed(4))
+        if (!Number.isInteger(squared)){
+            common_factor = Math.abs(greatest_common_factor(squared*multiplier, 1*multiplier))
+            numerator = (squared*multiplier)/common_factor
+            denominator = (1*multiplier)/common_factor
+            console.log(numerator)
+            fraction.innerHTML = `<sup>${numerator}</sup>&frasl;<sub>${denominator}</sub>`
+            calculator.display = fraction.textContent 
+        } else{
+            calculator.display = firstOperand ** 2
+        }
+        return firstOperand ** 2
+    }
+    return secondOperand
+}
+
+
 function handleOperator(secondOperator){
-    const {firstOperand, displayValue, operator} = calculator
-    const inputValue = parseFloat(displayValue)
-    if (operator && waitingForSecondOperand){
+    const {firstOperand, display, operator} = calculator
+    const inputValue = parseFloat(display)
+    if (operator === "**2") {
+        const result = calculate(firstOperand, inputValue, operator)
+        calculator.firstOperand = result
+    }
+    if (operator && calculator.waitingForSecondOperand){
         calculator.operator = secondOperator
         return
     }
-    if (firstOperand === null && (!isNan(inputValue))){
+    if (firstOperand === null && (!isNaN(inputValue))){
         calculator.firstOperand = inputValue
     } else if (operator){
         const result = calculate(firstOperand, inputValue, operator)
-        calculator.displayValue = `${parseFloat(result.toFixed(5))}`
         calculator.firstOperand = result
     }
     calculator.waitingForSecondOperand = true
     calculator.operator = secondOperator
 }
 
+function inputDecimal(dot){
+    if (calculator.waitingForSecondOperand === true){
+        calculator.display = "0."
+        calculator.waitingForSecondOperand = false
+        return
+    } if (!calculator.display.includes(dot)){
+        calculator.display += "."
+    }
+}
 
+function conversion(){
+    const {display} = calculator
+    if (display.includes(".")){
+        const multiplier = 10**(display.split(".")[1].length)
+        const numerator = (parseFloat(display)*multiplier)
+        const denominator = (1*multiplier)
+        const common_factor = Math.abs(greatest_common_factor(numerator, denominator))
+        fraction.innerHTML = `<sup>${numerator/common_factor}</sup>&frasl;<sub>${denominator/common_factor}</sub>`
+        calculator.display = fraction.textContent 
+    }
+    else {
+        const numerator = parseFloat(display.split("⁄")[0])
+        const denominator = parseFloat(display.split("⁄")[1])
+        calculator.display = (numerator/denominator).toString()
+    }
+}
+
+function resetCalculator(){
+    calculator.display = "0"
+    calculator.firstOperand = null
+    calculator.operator = null
+    calculator.waitingForSecondOperand = false
+}
+
+function updateDisplay(){
+    const display = document.querySelector("#calculator-screen")
+    display.value = calculator.display
+}
+
+updateDisplay()
 myButtons.addEventListener("click", (event) => {
     const {target} = event
     const {value} = target
-    if(!target.matches(button)){
+    if(!target.matches('button')){
         return;
     }
     switch (value){
-        case '+': 
+        case '+':
+        case '-':
+        case '*':
+        case '/': 
+        case '=': 
+        case '**2':
          handleOperator(value)
+         break
+        case '.':
+            inputDecimal(value)
+            break
+        case 'clr':
+            resetCalculator()
+            break
+        case 'convert':
+            conversion()
+            break
         default:
         if (Number.isInteger(parseFloat(value))){
             inputDigit(value)
         }
-    }
+    } 
+    updateDisplay()
+
 })
 
 
@@ -113,6 +249,7 @@ myButtons.addEventListener("click", (event) => {
   
 //       calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
 //       calculator.firstOperand = result;
+//       console.log(result)
 //     }
   
 //     calculator.waitingForSecondOperand = true;
@@ -128,8 +265,7 @@ myButtons.addEventListener("click", (event) => {
 //       return firstOperand * secondOperand;
 //     } else if (operator === '/') {
 //       return firstOperand / secondOperand;
-//     }
-//     return secondOperand;
+//     } return secondOperand
 //   }
   
 //   function resetCalculator() {
@@ -138,6 +274,7 @@ myButtons.addEventListener("click", (event) => {
 //     calculator.waitingForSecondOperand = false;
 //     calculator.operator = null;
 //   }
+
   
 //   function updateDisplay() {
 //     const display = document.querySelector('.calculator-screen');
@@ -169,10 +306,9 @@ myButtons.addEventListener("click", (event) => {
 //         resetCalculator();
 //         break;
 //       default:
-        
+//         if (Number.isInteger(parseFloat(value)))
 //           inputDigit(value);
-    
-//     }
+//         }
   
 //     updateDisplay();
 //   });
